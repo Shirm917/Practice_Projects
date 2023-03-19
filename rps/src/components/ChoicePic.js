9d4172062c7b97b4b2a7fe879968c4a8c925415e
Shir Markowitz <shirm91799@gmail.com>
2023-03-19 23:57:31 +0200

    added circles for icons and made the icons bigger

--- rps/src/components/ChoicePic.js
+++ rps/src/components/ChoicePic.js
@@ -5,15 +5,21 @@ import ContentCutIcon from "@mui/icons-material/ContentCut";
 const ChoicePic = (props) => {
   const { choice } = props;
 
-  return choice === "" ? (
-    null
-  ) : choice === "Rock" ? (
-    <CircleIcon />
-  ) : choice === "Paper" ? (
-    <NoteIcon />
-  ) : (
-    <ContentCutIcon />
-  );
+  return (
+    <div className="choicePic">
+      {
+        choice === "" ? (
+          null
+        ) : choice === "Rock" ? (
+          <CircleIcon sx={{fontSize: 130}} />
+        ) : choice === "Paper" ? (
+          <NoteIcon sx={{fontSize: 110}} />
+        ) : (
+          <ContentCutIcon sx={{fontSize: 100}} />
+        )
+      }
+    </div>
+  )
 };
 
 export default ChoicePic;
