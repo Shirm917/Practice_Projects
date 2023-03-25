dd6b167dae051fedd8037779f722020620c8e184
Shir Markowitz <shirm91799@gmail.com>
2023-03-25 21:10:29 +0300

    fixed ai function

--- tic-tac-toe/src/components/BoardSection.js
+++ tic-tac-toe/src/components/BoardSection.js
@@ -1,76 +1,62 @@
 import { useState, useEffect, useContext } from "react";
-import CloseIcon from "@mui/icons-material/Close";
-import PanoramaFishEyeIcon from "@mui/icons-material/PanoramaFishEye";
 import { AppContext } from "../App";
 
 const BoardSection = (props) => {
   const { turnNum, setTurnNum, resetBoard, setResetBoard, gameType } =
     useContext(AppContext);
   const { num } = props;
-  const [piece, setPiece] = useState(null);
+  const [piece, setPiece] = useState("");
   const [numArray, setNumArray] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8]);
   const [playerWent, setPlayerWent] = useState(false);
 
-  //  You get the parent and randomly get one of it's children
-  // but only one from the array,
-  // so it needs to be zero indexed or we can do plus 1.
   const versusAi = () => {
-    if (turnNum > 9 || piece !== null) {
+    if (turnNum > 9 || piece !== "") {
       return;
     } else if (turnNum % 2 === 1) {
-      setPiece(<CloseIcon sx={{ fontSize: 80 }} />);
+      setPiece("X");
       console.log("num", num);
-      //   const filteredNumArray = numArray.filter((element) => {
-      //     return element !== num;
-      //   })
-      //   console.log(filteredNumArray);
-      //   setNumArray(filteredNumArray);
-      const index = numArray.indexOf(num);
-      numArray.splice(index, 1);
-      setNumArray([...numArray]);
+      setNumArray(numArray.filter((element) => {
+        return element !== num;
+    }));
       setPlayerWent(true);
     }
     setTurnNum((num) => num + 1);
   };
 
   useEffect(() => {
+    console.log("playerWentUseEffect", numArray);
     if (playerWent) {
       console.log("numArray in useEffect", numArray);
       const randomNum = Math.floor(Math.random() * numArray.length);
       const aiChoice = numArray[randomNum];
-      console.log("aiChoice", aiChoice);
-      const index = numArray.indexOf(aiChoice);
-      numArray.splice(index, 1);
-      setNumArray([...numArray]);
-      // const filteredNumArray = numArray.filter((element) => {
-      //     return element !== aiChoice;
-      // });
-      // setNumArray(filteredNumArray);
+      setNumArray(numArray.filter((element) => {
+          return element !== aiChoice;
+      }));
       const board = document.querySelector(".board");
-      board.children[aiChoice].textContent = "o"
+      board.children[aiChoice].textContent = "O"
       setTurnNum((num) => num + 1);
     }
     setPlayerWent(false);
   }, [playerWent]);
 
-  useEffect(() => {
-    console.log("numArray", numArray);
-  }, [numArray]);
-
   const addPiece = () => {
-    if (turnNum > 9 || piece !== null) {
+    if (turnNum > 9 || piece !== "") {
       return;
     } else if (turnNum % 2 === 0) {
-      setPiece(<PanoramaFishEyeIcon sx={{ fontSize: 80 }} />);
+      setPiece("O");
     } else if (turnNum % 2 === 1) {
-      setPiece(<CloseIcon sx={{ fontSize: 80 }} />);
+      setPiece("X");
     }
     setTurnNum((num) => num + 1);
   };
 
   useEffect(() => {
     if (resetBoard) {
-      setPiece(null);
+      setPiece("");
+      const board = document.querySelector(".board");
+      for (const child of board.children) {
+        child.textContent = "";
+      }
     }
     setResetBoard(false);
     setNumArray([0, 1, 2, 3, 4, 5, 6, 7, 8]);
