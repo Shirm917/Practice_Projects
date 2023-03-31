d8664f52e29d74548a2990975432008e41487a03
Shir Markowitz <shirm91799@gmail.com>
2023-03-31 23:59:20 +0300

    creating project, downloading modules, and creating base of backend

---
+++ storefront/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Listening on ${process.env.PORT || 8080}`);
+})
