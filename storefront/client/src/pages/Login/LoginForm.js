b39209f90c830ae30e07ff9d0369216b33db0311
Shir Markowitz <shirm91799@gmail.com>
2023-04-01 23:12:02 +0300

    Created Login form

--- storefront/client/src/pages/Login/LoginForm.js
+++ storefront/client/src/pages/Login/LoginForm.js
@@ -2,7 +2,7 @@ import Box from "@mui/material/Box";
 import FormInput from "../../components/FormInput";
 import FormButton from "../../components/FormButton";
 
-const RegistrationForm = () => {
+const LoginForm = () => {
     return (
       <Box
         sx={{ display: "flex", flexDirection: "column", width: "50%", margin: "0 auto"}}
@@ -10,14 +10,11 @@ const RegistrationForm = () => {
         autoComplete="off"
         noValidate
       >
-        <FormInput label="First Name" />
-        <FormInput label="Last Name" />
-        <FormInput label="Email" />
-        <FormInput label="Username" />
+        <FormInput label="Username/Email" />
         <FormInput label="Password" />
-        <FormButton buttonText="Register"/>
+        <FormButton buttonText="Login"/>
       </Box>
     );
   };
 
-  export default RegistrationForm;
+  export default LoginForm;
