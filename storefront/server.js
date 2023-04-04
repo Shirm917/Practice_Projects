48cfd9471ff5d951a8bf3f066d1f2674501fd54e
Shir Markowitz <shirm91799@gmail.com>
2023-04-04 23:52:07 +0300

    added register user router and body parser to server file

--- storefront/server.js
+++ storefront/server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import users_router from "./routes/users.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(users_router);
 
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Listening on ${process.env.PORT || 8080}`);
