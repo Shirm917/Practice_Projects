9ea4c04bd4f20b8d4d7c8ad9cd283e041c32d4b6
Shir Markowitz <shirm91799@gmail.com>
2023-04-07 21:28:35 +0300

    added middleware to check data to registration route

--- storefront/routes/users.js
+++ storefront/routes/users.js
@@ -1,9 +1,10 @@
 import express from "express";
 import {register,login} from "../controllers/users.js";
+import { checkData } from "../middlewares/checkData.js";
 
 const router = express.Router();
 
-router.post("/register", register);
+router.post("/register", checkData, register);
 router.post("/login", login);
 
 export default router;
