7bb0a0c6789167bd55de3eb4fbbcca615377c646
Shir Markowitz <shirm91799@gmail.com>
2023-04-19 23:20:53 +0300

    making dialog responsive

--- storefront/client/src/pages/GroceryStore/ProductPopUp.js
+++ storefront/client/src/pages/GroceryStore/ProductPopUp.js
@@ -1,33 +1,30 @@
-import { useState } from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
 
 const ProductPopUp = (props) => {
-    const {open,setOpen, product} = props;
+  const { open, setOpen, product } = props;
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    return (
-        <Dialog
-        open={open}
-        onClose={handleClose}
-      >
-        <DialogTitle>
-          {product.name}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            {product.nutritional_info}
-          </DialogContentText>
-        </DialogContent>
-      </Dialog>
-     );
-}
+  return (
+    <Dialog fullScreen={fullScreen} open={open} onClose={handleClose}>
+      <DialogTitle>{product.name}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>{product.nutritional_info}</DialogContentText>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 export default ProductPopUp;
