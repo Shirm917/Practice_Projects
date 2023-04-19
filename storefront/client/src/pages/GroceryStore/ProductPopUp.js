5bc1060bafe51a014c5126db5134ffd178163d8f
Shir Markowitz <shirm91799@gmail.com>
2023-04-19 23:58:38 +0300

    putting content in the dialog popup

--- storefront/client/src/pages/GroceryStore/ProductPopUp.js
+++ storefront/client/src/pages/GroceryStore/ProductPopUp.js
@@ -7,6 +7,8 @@ import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useTheme } from "@mui/material/styles";
+import CloseIcon from "@mui/icons-material/Close";
+import pancakes from "../../assets/pancakes.jpg";
 
 const ProductPopUp = (props) => {
   const { open, setOpen, product } = props;
@@ -18,10 +20,53 @@ const ProductPopUp = (props) => {
   };
 
   return (
-    <Dialog fullScreen={fullScreen} open={open} onClose={handleClose}>
-      <DialogTitle>{product.name}</DialogTitle>
+    <Dialog
+      className="productPopUp"
+      fullScreen={fullScreen}
+      open={open}
+      onClose={handleClose}
+    >
+      <div className="popUpHeader">
+        <DialogActions>
+          <Button onClick={handleClose}>
+            <CloseIcon color="action" />
+          </Button>
+        </DialogActions>
+        <DialogTitle
+          className="popUpTitle"
+          sx={{ fontSize: 40, marginBottom: 7 }}
+        >
+          {product.name}
+        </DialogTitle>
+      </div>
+      <DialogContent className="popUpContent">
+        <img src={pancakes} />
+        <div>
+          <DialogContentText sx={{ color: "#000", fontSize: 20 }}>
+            ${product.price}
+          </DialogContentText>
+          <DialogContentText sx={{ color: "#000", fontSize: 20 }}>
+            {product.amount_per_price}
+          </DialogContentText>
+          <DialogContentText sx={{ color: "#000", fontSize: 20 }}>
+            {product.nutritional_info}
+          </DialogContentText>
+        </div>
+      </DialogContent>
+      <DialogActions>
+        <Button>Add to cart</Button>
+      </DialogActions>
       <DialogContent>
-        <DialogContentText>{product.nutritional_info}</DialogContentText>
+        <DialogContentText
+          sx={{ color: "#000", fontSize: 20, textAlign: "center" }}
+        >
+          About the Product
+        </DialogContentText>
+        <DialogContentText
+          sx={{ color: "#000", fontSize: 20, textAlign: "center" }}
+        >
+          {product.description}
+        </DialogContentText>
       </DialogContent>
     </Dialog>
   );
