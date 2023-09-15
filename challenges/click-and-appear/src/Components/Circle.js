1fe4ef991dfe4b4ea01b6d21356e2dec72e676eb
Shir Markowitz <shirm91799@gmail.com>
2023-09-15 23:28:40 +0100

    added a slight offset

--- challenges/click-and-appear/src/Components/Circle.js
+++ challenges/click-and-appear/src/Components/Circle.js
@@ -1,7 +1,7 @@
 const Circle = ({x,y,color,size}) => {
 
     return (
-        <div className="circle" style={{top: y + "px", left: x + "px", background: color, width: size + "px", height: size + "px"}}></div>
+        <div className="circle" style={{top: y - 5 + "px", left: x - 5 + "px", background: color, width: size + "px", height: size + "px"}}></div>
      );
 }
 
