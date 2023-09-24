ef7d0bb563c5bf83ffdbca6c6d8a17e9ffa485ff
Shir Markowitz <shirm91799@gmail.com>
2023-09-24 14:57:24 -0400

    creating memory game, card component, and game board component

---
+++ challenges/memory-game/src/Components/GameBoard.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Card from "./Card";
+
+const GameBoard = () => {
+    return (
+        <section className="game-board">
+
+        </section>
+     );
+}
+
+export default GameBoard;
