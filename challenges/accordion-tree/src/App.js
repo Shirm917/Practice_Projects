583a23025f528fe194511f89dcd5ec1bc63cb82b
Shir Markowitz <shirm91799@gmail.com>
2023-10-06 23:32:43 -0400

    added option to add subfolders on right click

--- challenges/accordion-tree/src/App.js
+++ challenges/accordion-tree/src/App.js
@@ -4,9 +4,15 @@ import "./App.css";
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   const [subFolders, setSubFolders] = useState(["Sub Folder 1", "Sub Folder 2"]);
+
+  const addSubFolder = () => {
+    const newSubFolder = prompt("Enter new sub folder name");
+    setSubFolders([...subFolders, newSubFolder]);
+  }
   return (
     <div className="folders">
-      <p onClick={() => setIsOpen(!isOpen)}>{isOpen ? "↓" : "→"} Main Folder</p>
+      <p>*Right click on main folder to add subfolders</p>
+      <p onClick={() => setIsOpen(!isOpen)} onContextMenu={addSubFolder}>{isOpen ? "↓" : "→"} Main Folder</p>
       {isOpen && (
         <div className="sub-folders">
           {subFolders.map((subFolder) => (
