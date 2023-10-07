6dc916777ab260e2d45971d0dfe6bc223896b4db
Shir Markowitz <shirm91799@gmail.com>
2023-10-06 23:27:19 -0400

    adding design to subfolders

--- challenges/accordion-tree/src/App.js
+++ challenges/accordion-tree/src/App.js
@@ -1,14 +1,17 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
+  const [subFolders, setSubFolders] = useState(["Sub Folder 1", "Sub Folder 2"]);
   return (
-    <div>
-      <p onClick={() => setIsOpen(!isOpen)}>Main Folder</p>
+    <div className="folders">
+      <p onClick={() => setIsOpen(!isOpen)}>{isOpen ? "↓" : "→"} Main Folder</p>
       {isOpen && (
-        <div>
-          <p>Sub Folder</p>
+        <div className="sub-folders">
+          {subFolders.map((subFolder) => (
+            <p>{subFolder}</p>
+          ))}
         </div>
       )}
     </div>
