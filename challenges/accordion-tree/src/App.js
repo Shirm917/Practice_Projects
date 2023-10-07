864ae1a445d71c352eadde331ceb1cbd1f19e3ba
Shir Markowitz <shirm91799@gmail.com>
2023-10-06 23:52:14 -0400

    changing sub folders to files

--- challenges/accordion-tree/src/App.js
+++ challenges/accordion-tree/src/App.js
@@ -3,20 +3,20 @@ import "./App.css";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
-  const [subFolders, setSubFolders] = useState(["Sub Folder 1", "Sub Folder 2"]);
+  const [files, setFiles] = useState(["File 1", "File 2"]);
 
-  const addSubFolder = () => {
-    const newSubFolder = prompt("Enter new sub folder name");
-    setSubFolders([...subFolders, newSubFolder]);
+  const addFile = () => {
+    const newFile = prompt("Enter new file name");
+    setFiles([...files, newFile]);
   }
   return (
     <div className="folders">
-      <p>*Right click on main folder to add subfolders</p>
-      <p onClick={() => setIsOpen(!isOpen)} onContextMenu={addSubFolder}>{isOpen ? "↓" : "→"} Main Folder</p>
+      <p>*Right click on main folder to add files</p>
+      <p onClick={() => setIsOpen(!isOpen)} onContextMenu={addFile}>{isOpen ? "↓" : "→"} Main Folder</p>
       {isOpen && (
-        <div className="sub-folders">
-          {subFolders.map((subFolder) => (
-            <p>{subFolder}</p>
+        <div className="files">
+          {files.map((file) => (
+            <p>{file}</p>
           ))}
         </div>
       )}
