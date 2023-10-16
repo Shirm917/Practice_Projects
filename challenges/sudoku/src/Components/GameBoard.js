7fa7bcc25f7bf0187845e44bfc2a2c32b53fc0c2
Shir Markowitz <shirm91799@gmail.com>
2023-10-15 22:57:41 -0400

    creating the layout of the board

--- challenges/sudoku/src/Components/GameBoard.js
+++ challenges/sudoku/src/Components/GameBoard.js
@@ -1,9 +1,31 @@
+import { useState } from "react";
+
 const GameBoard = () => {
-    return (
-        <div>
-            
-        </div>
-     );
-}
+  const [cells, setCells] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+
+  return (
+    <div className="game-board">
+      {cells.map((cell, i) => {
+        return (
+          <div className="row" key={i}>
+            {cell.map((subCell, j) => {
+              return <div className="cell" key={j}></div>;
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default GameBoard;
