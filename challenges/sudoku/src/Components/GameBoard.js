fba15aafbdf4b71869a26a960402b375592bea29
Shir Markowitz <shirm91799@gmail.com>
2023-10-18 00:06:27 -0400

    added ability to choose a number and place it into a cell

--- challenges/sudoku/src/Components/GameBoard.js
+++ challenges/sudoku/src/Components/GameBoard.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import NumberBtn from "./NumberBtn";
+import { logDOM } from "@testing-library/react";
 
 const GameBoard = () => {
   const [grid, setGrid] = useState([
@@ -17,6 +18,15 @@ const GameBoard = () => {
 
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+  const handleCellClick = (event) => {
+    const cellIndexString = event.target.id;
+    const columnIndex = parseInt(cellIndexString[0]);
+    const rowIndex = parseInt(cellIndexString[1]);
+    const gridCopy = [...grid];
+    gridCopy[columnIndex][rowIndex] = chosenNumber;
+    setGrid(gridCopy);
+  };
+
   return (
     <section className="game-board">
       <div className="sudoku-board">
@@ -28,8 +38,10 @@ const GameBoard = () => {
                 return (
                   <div
                     key={rowIndex}
+                    id={`${colIndex}${rowIndex}`}
                     style={{borderBottomWidth: dividerLineArr.includes(rowIndex) ? "5px" : "1px", borderRightWidth: dividerLineArr.includes(colIndex) ? "5px" : "1px" }}
                     className="cell"
+                    onClick={handleCellClick}
                   >
                     {number === 0 ? "" : number}
                   </div>
